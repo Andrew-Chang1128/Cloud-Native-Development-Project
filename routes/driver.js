c034de67402eb57972dee2604160abb38a87b25b
david <davidb891128@gmail.com>
2023-05-23 15:45:59 +0000

    change controller type from object to class

--- routes/driver.js
+++ routes/driver.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const controller = require("../controllers/driverController")
+const driverController = require("../controllers/driverController")
+
+controller = new driverController()
 router.get('/driver', controller.driver)
 router.get('/allDriver', controller.allDriver)
 module.exports = router
