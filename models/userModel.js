b3986e175fc6a2b14b0bf508f2eb2b6024692a3b
BuildTools <unconfigured@null.spigotmc.org>
2023-06-03 00:45:39 +0800

    [mongo] checkUserExist done

--- models/userModel.js
+++ models/userModel.js
@@ -1,28 +1,27 @@
-// mysql = require('mysql2');
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://mongo:27017/";
 
 module.exports = class {
     constructor() {
-        // this.connection = mysql.createConnection({
-        //     host: 'localhost',
-        //     user: 'test',
-        //     password: 'password',
-        //     database: 'STproject',
-        // });
+        this.client = new MongoClient(uri);
     };
 
     checkUserExist(email) {
         console.log("checking user exist with: ", email);
-        return false;
-        return new Promise((resolve, reject) => {
-            // this.connection.query(query, [username, password], (err, result) => {
-            //   if (err) {
-            //       console.error('Error inserting user information:', err);
-            //       reject(false);
-            //   }else{
-            //       resolve(true);
-            //   }
-            // });
-            // reject(false);
+        return new Promise(async (resolve, reject) => {
+            try {
+                const database = this.client.db('cnp');
+                const user = database.collection('user');
+                const query = { email: email };
+                console.log('query', query);
+                const data = await user.findOne(query);
+                //console.log('data', data);
+                if(!data) resolve(false);
+                resolve(true);
+            } catch (err) {
+                console.error('Error:', err);
+            }
+            reject(false);
         })
     }
 
