a31f9180591ecc6c9a0554377778aaa5be2a3118
exodustw <windows44444444@gmail.com>
2023-06-04 00:22:37 +0800

    orderModel complete

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -10,28 +10,50 @@ module.exports = class {
     };
 
     addPassengerToOrder(passengerId, routeId, datetime, numOfPassenger, start, end, fee) {
-        console.log("Adding Passenger", passengerId, " to ", routeId, datetime, numOfPassenger, start, end);
+        console.log("Adding Passenger", passengerId, " to ", routeId, datetime, numOfPassenger, start, end, fee);
         return new Promise(async (resolve, reject) => {
             try {
+                var dstart = new Date(datetime);
+                dstart.setHours(0,0,0,0);
+                var dend = new Date(dstart);
+                dend.setDate(dstart.getDate()+1);
                 const order = this.database.collection('order');
                 const query = {
                     routeId: routeId,
-                    
+                    datetime: {
+                        $gte: dstart.toISOString(),
+                        $lt : dend.toISOString()
+                    }
                 };
-                const row = await order.findOne(query).toArray();
-                const id = row[0].routeId + 1;
-                const data = {
-                    routeId: id,
-                    dayOfWeek: dayOfWeek,
-                    maxNumOfPassenger: maxNumOfPassenger,
-                    startTime: startTime,
-                    routeList: routeList
-                };
-                console.log('data', data);
-                const result = await route.insertOne(data);
+                const row = await order.findOne(query);
+                console.log("row", row);
+                if(!row) {
+                    resolve(false);
+                    return;
+                }
+                // const id = row[0].routeId + 1;
+
+                const command = {
+                    $push: {
+                        passenger: {
+                            subOrderId: 0,
+                            passengerId: passengerId,
+                            numOfPassenger: numOfPassenger,
+                            start: start,
+                            end: end,
+                            timestamp: new Date().toISOString(),
+                            fee: fee
+                        }
+                    }
+                }
+                const options = { upsert: false };
+                const result = await order.updateOne(query, command, options);
                 console.log('result', result);
-                if(!result.acknowledged) resolve(-1);
-                resolve(sid);
+                // if(!result.acknowledged) resolve(-1);
+                // resolve(sid);
+                if (!result.acknowledged) resolve(false);
+                else resolve(true);
+                return;
             } catch (err) {
                 console.error('Error:', err);
             }
@@ -41,49 +63,100 @@ module.exports = class {
 
     getPassengerAllOrder(passengerId) {
         console.log("Fetching all orders of passenger: ", passengerId);
-        return [{
-            orderId: 0, routeId: 1, datetime: "2021-05-01 10:00:00",
-            passenger: [
-                {
-                    subOrderId: 10, passengerId: passengerId, numOfPassenger: 2,
-                    fee: 300, start: { loc: "起點", lat: 123, lng: 456 },
-                    end: { loc: "終點", lat: 321, lng: 456 },
-                    timestamp: "2021-05-01 10:00:00"
-                },
-                {
-                    subOrderId: 11, passengerId: passengerId, numOfPassenger: 1,
-                    fee: 700, start: { loc: "起點", lat: 123, lng: 456 },
-                    end: { loc: "終點", lat: 321, lng: 456 },
-                    timestamp: "2021-05-10 10:00:00"
-                }
-            ]
-        }]
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const order = this.database.collection('order');
+                // const query = {
+                //     passenger: {
+                //         $elemMatch: 
+                //         {
+                //             passengerId: passengerId
+                //         }
+                //     }
+                // };
+                const pipeline = [
+                    {
+                        $match: {
+                            passenger: {
+                                $elemMatch: {
+                                    passengerId: passengerId
+                                }
+                            }
+                        }
+                    },
+                    {
+                        $addFields: {
+                            passenger: {
+                                $filter: {
+                                    input: "$passenger",
+                                    cond: {
+                                        $eq: [
+                                            "$$this.passengerId",
+                                            passengerId
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    }
+                ];
+                const data = await order.aggregate(pipeline).toArray();
+                console.log('data', data);
+                if(!data) resolve([]);
+                else resolve(data);
+                return;
+            } catch (err) {
+                console.error('Error:', err);
+            }
+            reject(false);
         })
     }
 
     getDriverAllOrder(driverId) {
         console.log("Fetching all orders of driver: ", driverId);
-        const routeModel = new rModel();
-        const allDiverRoute = routeModel.getDiverRoute(driverId);
-        return [{
-            orderId: 0, routeId: 1, datetime: "2021-05-01 10:00:00",
-            passenger: [
-                {
-                    subOrderId: 10, passengerId: 1, numOfPassenger: 2,
-                    fee: 300, start: { loc: "起點", lat: 123, lng: 456 },
-                    end: { loc: "終點", lat: 321, lng: 456 },
-                    timestamp: "2021-05-01 10:00:00"
-                },
-                {
-                    subOrderId: 11, passengerId: 2, numOfPassenger: 1,
-                    fee: 700, start: { loc: "起點", lat: 123, lng: 456 },
-                    end: { loc: "終點", lat: 321, lng: 456 },
-                    timestamp: "2021-05-10 10:00:00"
-                }
-            ]
-        }]
-        return new Promise((resolve, reject) => {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const route = this.database.collection('route');
+                const pipeline = [
+                    {
+                        $match: {
+                            'driverId': driverId
+                        }
+                    }
+                    ,
+                    {
+                        $lookup: {
+                            from: "order",
+                            localField: "routeId",
+                            foreignField: "routeId",
+                            as: "order"
+                        }
+                    }
+                    , 
+                    {
+                        $project: {
+                            _id: 0,
+                            order: 1
+                        }
+                    },
+                    {
+                        $unwind: "$order" 
+                    },
+                    {
+                        $replaceRoot: {
+                            newRoot: "$order" 
+                        }
+                    }
+                ];
+                const data = await route.aggregate(pipeline).toArray();
+                console.log('data', data);
+                if(!data) resolve([]);
+                else resolve(data);
+                return;
+            } catch (err) {
+                console.error('Error:', err);
+            }
+            reject(false);
         })
     }
 }
