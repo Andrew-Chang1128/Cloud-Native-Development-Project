de3db0c909bef19816a80d33f131c03d58a86639
benny12312311 <benny12312311@gmail.com>
2023-06-03 21:55:48 +0800

    frontend first time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Header from './Header';
+import Menu from './Menu';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
 import Signin from './page/Signin';
 import Profile from './page/Profile';
@@ -10,25 +10,28 @@ import Payments from './page/Payments';
 import Order from './page/Order';
 import Ordercomplete from './page/Ordercomplete';
 import Ordernext from './page/Ordernext';
+
 function App() {
   return (
    <BrowserRouter>
-      <div className="rectangle-background">
-        <p className="text">TSMC Uber</p>
-     </div>
-    <Routes>
-      <Route path="/" element={<Header />}></Route>
-      <Route path='/order' element={<Order />}></Route>
-      <Route path='/signin' element={<Signin />}></Route>
-      <Route path='/profile' element={<Profile />}></Route>
-      <Route path='/profilechange' element={<Profilechange />}></Route>
-      <Route path='/payments' element={<Payments/>}></Route>
-      <Route path='/Ordersearch' element={<Ordersearch />}></Route>
-      <Route path='/askhelp' element={<Askhelp />}></Route>
-      <Route path='/ordercomplete' element={<Ordercomplete />}></Route>
-      <Route path='/ordernext' element={<Ordernext />}></Route>
-    </Routes>
-
+    <div className="App">
+      <div className="header">
+        <p className="header-text">TSMC Uber</p>
+      </div>
+      <Routes>
+        <Route path="/" element={<Signin />}></Route>
+        <Route path="/menu" element={<Menu />}></Route>
+        <Route path='/order' element={<Order />}></Route>
+        <Route path='/signin' element={<Signin />}></Route>
+        <Route path='/profile' element={<Profile />}></Route>
+        <Route path='/profilechange' element={<Profilechange />}></Route>
+        <Route path='/payments' element={<Payments/>}></Route>
+        <Route path='/Ordersearch' element={<Ordersearch />}></Route>
+        <Route path='/askhelp' element={<Askhelp />}></Route>
+        <Route path='/ordercomplete' element={<Ordercomplete />}></Route>
+        <Route path='/ordernext' element={<Ordernext />}></Route>
+      </Routes>
+    </div>
    </BrowserRouter>
          
   );
---
+++ frontend/src/Menu.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import profileImage from './image/people.png';
+import { useNavigate } from 'react-router-dom';
+import signinImage from './image/signin.png'
+import orderImage from './image/order.png'
+import './App.css'
+
+function Menu() {
+    const navigate = useNavigate();
+    return (
+     <>
+        <div className="map">
+            <p> map </p>
+        </div>
+        <div className="menu-gesture">
+            <button onClick={() => navigate('/signin')} style={{ background: 'none', border: 'none', padding: 0 }}>
+                <img className="ges-icon" src={signinImage} alt="Signin" />
+            </button>
+            <button onClick={() => navigate('/order')} style={{ background: 'none', border: 'none', padding: 0 }}>
+                <img className="ges-icon" src={orderImage} alt="Order" />
+            </button >
+            <button onClick={() => navigate('/profile')} style={{ background: 'none', border: 'none', padding: 0 }}>
+                <img className="ges-icon" src={profileImage} alt="Profile" />
+            </button>
+        </div>
+    </>
+    );
+}
+
+export default Menu;
