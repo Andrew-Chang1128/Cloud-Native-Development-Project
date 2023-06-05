07b332a48c30def07cbe270413972f39d17e3605
Corgi87 <ter66528211@gmail.com>
2023-06-06 01:31:25 +0800

    Askhelp modified

--- frontend/src/page/Askhelp.js
+++ frontend/src/page/Askhelp.js
@@ -9,29 +9,27 @@ function Askhelp(){
     window.location.replace('tel:+886-900-000-000');
   };
   return (
-    <div>
-
-      <button onClick={() => navigate('/')} style={{ background: 'gray', border: 'none', padding: 0,position: 'absolute', top: '575px', left: '75px', width: '250px', height: '50px'  }}>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <img style={{ width: '50px', height: '50px' }} src={messageImage} alt="Message" />
-              <span style={{ marginLeft: '50px', color: 'black'}}>傳送訊息給司機</span>
-            </div>
-      </button>
-      <button onClick={handleClick} style={{ background: 'gray', border: 'none', padding: 0,position: 'absolute', top: '650px', left: '75px', width: '250px', height: '50px'  }}>
-            <div style={{ display: 'flex', alignItems: 'center' }}>
-              <img style={{ width: '50px', height: '50px' }} src={cellphoneImage} alt="Cellphone" />
-              <span style={{ marginLeft: '50px', color: 'black'}}>致電給司機</span>
-            </div>
-      </button>
-
-      <div style={{ position: 'absolute', top: '744px', left: '0px', width: '390px', height: '100px' }}>
-        <div style={{ backgroundColor: 'gray', width: '100%', height: '100%' }}>
+    <>
+    <div className="content" style={{ "flex-direction": "column" }}>
+          <div className="profile-button">
+            <button onClick={() => navigate('/menu')}>
+              <img src={messageImage} alt="Message" />
+              <span>傳送訊息給司機</span>
+            </button>
+            <button onClick={handleClick}>
+                <img src={cellphoneImage} alt="Cellphone" />
+                <span>致電給司機</span>
+            </button>
+            
+          </div>
+        </div>
+        
+        <div className="menu-gesture">
           <button onClick={() => window.history.back()} style={{ background: 'none', border: 'none', padding: 0 }}>
-            <img style={{ width: '25px', height: '50px', position: 'absolute', top: '20px', left: '180px' }} src={buttonImage} alt="Back" />
+              <img className="ges-icon" src={buttonImage} alt="Back" />
           </button>
         </div>
-      </div>
-    </div>
+    </>
   );
 }
 
