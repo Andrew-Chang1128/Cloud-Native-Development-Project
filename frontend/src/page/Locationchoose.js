0cf0c78f493a9e696786cdac6075dfc214c5a228
exodustw <windows44444444@gmail.com>
2023-06-07 00:11:37 +0800

    pass geo to parent

--- frontend/src/page/Locationchoose.js
+++ frontend/src/page/Locationchoose.js
@@ -4,17 +4,25 @@ import markImage from '../image/mark.png'
 import { useNavigate, useLocation} from 'react-router-dom';
 import checkImage from '../image/check.png'
 function Locationchoose(){
+  const ref = React.useRef();
+
   const navigate = useNavigate();
   const location = useLocation();
   console.log(location.state);
+
+  function click(){
+    var cnt = ref.current.map.getCenter();
+    console.log('CC Center at ', cnt.lat, cnt.lng);
+    navigate('/order', {state:{id:2,name:'hehe'}});
+  }
   return (
     <div style={{ position: 'relative' }}>
-      <LocationChooseMap />
+      <LocationChooseMap ref={ref}/>
       <div>
         <img src={markImage} height="60" style={{ height: '80',  position: 'absolute',  top: '45%',  left: '45%'}}/>
       </div>
       <div className="menu-gesture">
-        <button onClick={() => navigate('/order', {state:{id:2,name:'hehe'}})} style={{ background: 'none', border: 'none', padding: 0}}>
+        <button onClick={() => click()} style={{ background: 'none', border: 'none', padding: 0}}>
         <img className="ges-icon" src={checkImage} alt="Check" />
           </button>
         </div>
