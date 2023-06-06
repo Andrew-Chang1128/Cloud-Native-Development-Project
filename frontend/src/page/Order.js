605dd6a6a46d5e6d5cc10849ef75c23f0970083c
Corgi87 <ter66528211@gmail.com>
2023-06-06 23:32:11 +0800

    order modified

--- frontend/src/page/Order.js
+++ frontend/src/page/Order.js
@@ -5,26 +5,33 @@ import buttonImage from '../image/back.png';
 import apiImage from '../image/api.png';
 function Order(){
   const navigate = useNavigate();
-  const [selectedValue, setSelectedValue] = useState("1");
+  const [selectedValue, setSelectedValue] = useState('');
   const handleChange = (e) => {
     setSelectedValue(e.target.value);
   }
-
+  const [depart, setDepart] = useState('');
+  const handleInputDepartChange = (depart) => {
+    setDepart(depart.target.value);
+  };
+  const [destination, setDestination] = useState('');
+  const handleInputChange = (event) => {
+    setDestination(event.target.value);
+  };
     return (
       <>
       <div className="content" style={{ "flex-direction": "column" }}>
           <div className="profile-div" style={{ "flex-direction": "column" }}>
             <p style={{ fontSize: "3vh", paddingBottom: "1vw" }}>出發地</p>
-            <button onClick={() => navigate('/locationchoose')} style={{ width: '85vw', marginLeft: "7.5vw",backgroundColor: "#D9D9D9"}}>
+            <button onClick={() => navigate('/locationchoose')} style={{ width: '10vw', marginLeft: "7.5vw",backgroundColor: "#D9D9D9"}}>
                 <img src={apiImage} alt="api1" />
-              <span>交通大學</span>
             </button>
+            <input type="text" id="depart" name="topping2" value={depart} onChange={handleInputDepartChange} style={{ fontSize: "1vh",width: '60vw',marginLeft: "7.5vw" }} />
             <p style={{ fontSize: "3vh" , paddingBottom: "1vw"}}>目的地</p>
             <div>
               <button onClick={() => navigate('/locationchoose')} style={{ width: '10vw', marginLeft: "75vw",backgroundColor: "#D9D9D9"}}>
                 <img src={apiImage} alt="api1" />
               </button>
-              <input type="textbox" id="destination" name="topping3" value="" style={{ fontSize: "1vh",width: '85vw',marginLeft: "7.5vw" }} />
+              <input type="text" id="destination" name="topping3" value={destination} onChange={handleInputChange} style={{ fontSize: "1vh",width: '60vw',marginLeft: "7.5vw" }} />
             </div>
             
             
