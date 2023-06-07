e89e62cde70ce81fa2f15ff28f1fdde2b98e16b1
exodustw <windows44444444@gmail.com>
2023-06-07 17:26:34 +0800

    backend model fix

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -34,7 +34,11 @@ module.exports = class {
             try {
                 const user = this.database.collection('user');
                 const row = await user.find({}).sort({ "id": -1 }).limit(1).toArray();
-                const id = (row[0].id + 1) || 1;
+                console.log(row);
+                var id = 1;
+                if(row.length > 0){
+                    id = (row[0].id + 1) || 1;
+                }
                 const data = {
                     id: id,
                     name: name,
