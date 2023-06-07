f67877d4da069a1abc23340e19cdd6acdf1c7a4a
exodustw <windows44444444@gmail.com>
2023-06-07 13:12:19 +0800

    fix endpoint /user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const controller = require("../controllers/userController")
+const authentication = require("../utils/authentication")
 
 const userController = new controller()
-router.post('/login', userController.login)
-router.post('/createUser', userController.createUser)
+router.post('/login', userController.login);
+router.post('/createUser', userController.createUser);
+router.get('/', authentication.auth, userController.user);
 module.exports = router
